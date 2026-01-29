5530a85d75905620196de2d6e0f171d12bbc718d
Alvf <alvin.shi@yale.edu>
2026-01-29 14:21:12 -0500

    strandViewer prepped for actual coding...

--- src/modules/STRANDVIEWER.h
+++ src/modules/STRANDVIEWER.h
@@ -3,5 +3,7 @@
 
 // each object/UI element pair has a SINGLE corresponding struct
 // which contains all of the state needed to keep it running
+
 // along with some method for modifying state (to be called within uiElements(const ImGuiIO& io))
+
 // and another for drawing (to be called within rendering() of app thing)
